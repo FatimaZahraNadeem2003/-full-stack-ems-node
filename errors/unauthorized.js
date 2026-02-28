4c2eeadb25b2907fbb413cc4628b5ddad9f93678
Fatima <bc220417002fzn@vu.edu.pk>
2026-03-01 01:22:54 +0500

    status code 403

--- errors/unauthorized.js
+++ errors/unauthorized.js
@@ -4,7 +4,7 @@ const CustomAPIError = require('./custom-api');
 class UnauthorizedError extends CustomAPIError {
     constructor(message) {
         super(message);
-        this.statusCode = StatusCodes.FORBIDDEN; // 403
+        this.statusCode = StatusCodes.FORBIDDEN; 
     }
 }
 
