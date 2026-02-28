0b93cb74dcb2e8b62ed699287252c43b7db18daf
Fatima <bc220417002fzn@vu.edu.pk>
2026-03-01 02:01:53 +0500

    added validations for teacher

--- middleware/validation.js
+++ middleware/validation.js
@@ -25,7 +25,38 @@ const validateStudent = [
     next();
   }
 ];
+const validateTeacher = [
+  body('firstName').notEmpty().withMessage('First name is required')
+    .isLength({ min: 2, max: 50 }).withMessage('First name must be 2-50 characters'),
+  
+  body('lastName').notEmpty().withMessage('Last name is required')
+    .isLength({ min: 2, max: 50 }).withMessage('Last name must be 2-50 characters'),
+  
+  body('email').isEmail().withMessage('Valid email is required')
+    .normalizeEmail(),
+  
+  body('employeeId').notEmpty().withMessage('Employee ID is required')
+    .isAlphanumeric().withMessage('Employee ID must be alphanumeric'),
+  
+  body('qualification').notEmpty().withMessage('Qualification is required'),
+  
+  body('specialization').notEmpty().withMessage('Specialization is required'),
+  
+  body('contactNumber').notEmpty().withMessage('Contact number is required')
+    .isMobilePhone().withMessage('Valid contact number required'),
+  
+  body('experience').optional().isInt({ min: 0 }).withMessage('Experience must be a positive number'),
+  
+  (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      throw new BadRequestError(errors.array()[0].msg);
+    }
+    next();
+  }
+];
 
 module.exports = {
-  validateStudent
+  validateStudent,
+  validateTeacher
 };
