9ca381b85e3bd6f42ccf1bd5ad14b4952fc7be08
Fatima <bc220417002fzn@vu.edu.pk>
2026-03-01 01:18:17 +0500

    Routes

--- app.js
+++ app.js
@@ -44,7 +44,6 @@ app.use(xss());
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 
-// Routes
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/test", testRoutes);
